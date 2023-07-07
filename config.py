f73336e22d4bbdc9c87c8a2b054888cfea0d7471
Hamed Gorgini <reg.hamiya@gmail.com>
2023-07-07 21:07:28 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 
 # ===== ( Config Bot V2ray ) =====
 
-TOKEN = "" # Token Bot ==> @BotFather
+TOKEN = "6277285862:AAHLYJpJn_gCxXnX-cdgBZ3R_CfMizT5si8" # Token Bot ==> @BotFather
 
 Api_id,Api_hash = 6,"eb06d4abfb49dc3eeb1aeb98ae0f581e"   # Api_id,Api_hash ==> https://my.telegram.org/auth
 
 channels = [ # only public channels
-    "@"
+    "@vpn2forosh"
 ] # Channels to join and bot admins in channels
 
-admins = [1111,2222,3333] # Admins bot ==> command : /panel
+admins = [5560376628,2222,3333] # Admins bot ==> command : /panel
 
 timefree = 120 # Wait time until the next configuration in seconds
 
