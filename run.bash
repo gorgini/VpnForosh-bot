2b750a261e73ba345d8fcd53167204edcdd091ad
Hamed Gorgini <reg.hamiya@gmail.com>
2023-07-07 21:15:20 +0330

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo apt-get install wget
-wget https://github.com/virnow/V2ray-config-bot/archive/refs/heads/main.zip
+wget https://github.com/gorgini/VpnForosh-bot/archive/refs/heads/main.zip
 sudo apt-get install unzip
 unzip main.zip
 sleep 2
 sudo apt-get install python3-pip
-pip3 install -r V2ray-config-bot-main/requirements.txt
+pip3 install -r VpnForosh-bot/requirements.txt
 echo "Edit config File"
 sleep 2
-nano V2ray-config-bot-main/config.py
+nano VpnForosh-bot/config.py
 echo "Now Run Bot in background"
 sleep 2
-(cd V2ray-config-bot-main && python3 main.py &)
-(cd V2ray-config-bot-main && python3 update.py &)
+(cd VpnForosh-bot-main && python3 main.py &)
+(cd VpnForosh-bot-main && python3 update.py &)
 echo "End"
 exit
